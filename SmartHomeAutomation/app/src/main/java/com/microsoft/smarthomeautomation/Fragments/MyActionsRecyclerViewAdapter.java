9b013654abcc784d28841ab5ceb68cbfa5956039
wishel <wishel@microsoft.com>
2017-07-25 11:09:24 -0700

    UI work

--- SmartHomeAutomation/app/src/main/java/com/microsoft/smarthomeautomation/Fragments/MyActionsRecyclerViewAdapter.java
+++ SmartHomeAutomation/app/src/main/java/com/microsoft/smarthomeautomation/Fragments/MyActionsRecyclerViewAdapter.java
@@ -1,13 +1,19 @@
 package com.microsoft.smarthomeautomation.Fragments;
 
+import android.animation.ObjectAnimator;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
-import com.microsoft.smarthomeautomation.Fragments.ActionsFragment.OnListFragmentInteractionListener;
 import com.microsoft.smarthomeautomation.DTO.Actions.Action;
+import com.microsoft.smarthomeautomation.Fragments.ActionsFragment.OnListFragmentInteractionListener;
 import com.microsoft.smarthomeautomation.R;
 
 import java.util.List;
@@ -35,20 +41,40 @@ public class MyActionsRecyclerViewAdapter extends RecyclerView.Adapter<MyActions
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).ReadableName);
-        holder.mContentView.setText(mValues.get(position).Type);
+        final Action selectedItem = mValues.get(position);
+        holder.mItem = selectedItem;
+        holder.icon.setImageResource(getResourceForType(selectedItem.Type));
+        holder.title.setText(selectedItem.ReadableName);
+        holder.title.setTypeface(null, Typeface.NORMAL);
+        holder.enabledSwitch.setChecked(selectedItem.Enabled);
+        holder.enabledSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                selectedItem.Enabled = isChecked;
+            }
+        });
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
+                MyActionsRecyclerViewAdapter.this.notifyDataSetChanged();
+                if (holder.containerLayout.getVisibility() == View.GONE) {
+                    holder.title.setTypeface(null, Typeface.BOLD);
+                    holder.containerLayout.setVisibility(View.VISIBLE);
+                    // TODO: replace containerLayout's children with custom view
+                } else {
+                    holder.title.setTypeface(null, Typeface.NORMAL);
+                    holder.containerLayout.setVisibility(View.GONE);
                 }
+
+                ObjectAnimator animation = ObjectAnimator.ofInt(holder.containerLayout, "Bottom", holder.containerLayout.getTop());
+                animation.setDuration(200).start();
             }
         });
+    }
+
+    private int getResourceForType(String type) {
+        return 0;
     }
 
     @Override
@@ -58,20 +84,19 @@ public class MyActionsRecyclerViewAdapter extends RecyclerView.Adapter<MyActions
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
+        public final ImageView icon;
+        public final TextView title;
+        public final Switch enabledSwitch;
+        public final FrameLayout containerLayout;
         public Action mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.id);
-            mContentView = (TextView) view.findViewById(R.id.content);
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            icon = (ImageView) view.findViewById(R.id.icon);
+            title = (TextView) view.findViewById(R.id.content);
+            enabledSwitch = (Switch) view.findViewById(R.id.enabled_switch);
+            containerLayout = (FrameLayout) view.findViewById(R.id.container_layout);
         }
     }
 }
